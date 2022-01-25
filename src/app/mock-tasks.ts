8f3e42ab031e026af5b04d2b668ea60fb27fe4f5
GermanCleiman <cleimangerman@gmail.com>
2022-01-25 18:05:41 -0300

    Agrego mock de tareas y el interfece

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,31 @@
+import { Task } from "./Task"
+
+export const TASK: Task[] = [
+  {
+    id: 1,
+    text: "Terminar el primer modulo de Angular",
+    day: 'Enero 26 a las 12hs',
+    reminder: true
+  },
+
+  {
+    id: 2,
+    text: "Comprar antipulgas gatos",
+    day: 'Enero 25 a las 19hs',
+    reminder: true
+  },
+
+  {
+    id: 3,
+    text: "Investigar sobre Bootstrap",
+    day: 'Enero 26 a las 15hs',
+    reminder: false
+  },
+
+  {
+    id: 4,
+    text: "Leer un libro",
+    day: 'Enero 26 a las 22hs',
+    reminder: false
+  }
+]
